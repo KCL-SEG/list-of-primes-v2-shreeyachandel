77a49dc4a3a8a992c2ad6f84315e63e84af32686
Shreeya Chandel <shreeyachandel7@gmail.com>
2023-12-01 20:52:49 +0000

    Pass all tests

--- primes.py
+++ primes.py
@@ -1,6 +1,24 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def is_prime(num):
+    if num<2:
+        return False
+    for i in range (2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
 def primes(number_of_primes):
+    if number_of_primes <=0:
+        raise ValueError("Number of primes must be positive")
+
     list = []
+    num = 2
+    while len(list) < number_of_primes:
+        if is_prime(num):
+            list.append(num)
+        num +=1
+    
     return list
